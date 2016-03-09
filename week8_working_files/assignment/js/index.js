9a0fda9fa0b32c3d3d2310d5ab41c052e9ef85c1
haikelzaini <you@example.com>
2016-03-09 21:46:31 +0800

    assignment

---
+++ week8_working_files/assignment/js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('#form-phone').blur(function(e) {
+        if (validatePhone('form-phone')) {
+            $('#spnPhoneStatus').html('Valid');
+            $('#spnPhoneStatus').css('color', 'green');
+        }
+        else {
+            $('#spnPhoneStatus').html('Invalid');
+            $('#spnPhoneStatus').css('color', 'red');
+        }
+    });
+});
+
+function validatePhone(txtPhone) {
+    var a = document.getElementById(txtPhone).value;
+    var filter = /^[0-9-+]+$/;
+    if (filter.test(a)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
